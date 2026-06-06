3602f3a9f80dc5c1985134751e874f6378f7bc08
Liu Zhening <129078862+HYPERVAPOR@users.noreply.github.com>
2026-06-06 21:54:25 +0800

    feat: add double-Esc to exit terminal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,6 @@ vim.keymap.set("v", "<C-/>", "gc", { remap = true, desc = "Comment selection" })
 
 -- （可选）把终端功能挪到 Ctrl+\，这样两个功能都能保留
 vim.keymap.set({"n", "t"}, "<C-\\>", function() Snacks.terminal.focus(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
+
+-- Terminal 模式快速退出到 Normal 模式
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
