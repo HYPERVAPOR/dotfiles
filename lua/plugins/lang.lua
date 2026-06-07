b882b84fc52567b762838bbdf2321de32f79ec2d
Liu Zhening <129078862+HYPERVAPOR@users.noreply.github.com>
2026-06-07 19:19:57 +0800

    fix: move LazyVim extras to lazyvim.json to fix import order

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,31 +1,5 @@
 return {
-  -- TypeScript / JavaScript（已有，保留显式声明）
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-
-  -- Python
-  { import = "lazyvim.plugins.extras.lang.python" },
-
-  -- Go
-  { import = "lazyvim.plugins.extras.lang.go" },
-
-  -- Rust
-  { import = "lazyvim.plugins.extras.lang.rust" },
-
-  -- Tailwind CSS
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-
-  -- C / C++
-  { import = "lazyvim.plugins.extras.lang.clangd" },
-
-  -- JSON / YAML / TOML（常用配置文件）
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.lang.toml" },
-
-  -- Markdown
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-
-  -- 补充 CSS LSP（LazyVim 没有单独的 CSS extra）
+  -- 补充 CSS / HTML LSP（LazyVim 没有单独的 CSS/HTML extra）
   {
     "mason-org/mason.nvim",
     opts = {
